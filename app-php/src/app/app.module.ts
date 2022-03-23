928212c5fbb99786835cebcb210ad73ceeaace96
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-23 16:25:28 -0300

    3.F: ngOnInit

--- app-php/src/app/app.module.ts
+++ app-php/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule} from '@angular/common/Http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CursoComponent } from './curso/curso.component';
