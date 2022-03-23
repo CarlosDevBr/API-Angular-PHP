e407fbb5264566e54259a0267db6eb3396457783
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-23 16:56:03 -0300

    4.G: Created constructor of service and import map from rxjs

--- app-php/src/app/curso/curso.service.ts
+++ app-php/src/app/curso/curso.service.ts
@@ -1,9 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 }
