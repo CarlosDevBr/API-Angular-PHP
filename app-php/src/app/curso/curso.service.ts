0ab23c386014f264c013a3e0e4e6a789d07efb1b
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-24 10:57:11 -0300

    7.A: Created service remove

--- app-php/src/app/curso/curso.service.ts
+++ app-php/src/app/curso/curso.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
 import { map } from 'rxjs/operators';
 import { Curso } from './curso';
 
@@ -32,6 +33,23 @@ export class CursoService {
     .pipe(map((res) => {
       this.vetor.push(res['cursos']);
       return this.vetor;
+    }))
+  }
+
+  //Remover curso
+  removerCurso(c:Curso):observable<Curso[]>{
+
+    const param = new HttpParams().set("idCurso", c.idCurso?.toString());
+
+    return this.http.delete(this.url+'excluir', {params:params})
+    .pipe(map((res) => {
+
+      const filtro = this.vetor.filter((curso) => {
+        return +curso['idCurso'] !== +c.idCurso;
+      });
+
+      return this.vetor = filtro;
+
     }))
   }
 }
